d0afeca741d2356973319cdcdcaa657c5d979362
Katie McLaughlin <katie@glasnt.com>
2019-03-10 09:18:47 +1100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements.txt", "r") as f: 
-    requirements = f.read()
+# TODO - fix so this works
+#with open("requirements.txt", "r") as f: 
+#    requirements = f.read()
 
 setuptools.setup(
     name="ih",
-    version="0.0.0-dev2",
+    version="0.0.0-dev3",
     author="katie McLaughlin",
     author_email="katie@glasnt.com",
     description="A very persuasive package, for creating embroidery patterns",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/glasnt/ih",
-    install_requires=requirements,
+    install_requires=['click', 'pillow'],
     entry_points={
         'console_scripts': [
             "ih = ih.__main__:main"
