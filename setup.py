cbfeb89bebf14f5b85a8e3f28e32121ccada422d
Katie McLaughlin <katie@glasnt.com>
2019-04-27 14:50:39 +1000

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="ih",
-    version="0.0.0-dev21",
+    version="0.0.0-dev22",
     author="Katie McLaughlin",
     author_email="katie@glasnt.com",
     description="A very persuasive package, for creating embroidery patterns",
