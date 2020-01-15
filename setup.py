cd048b21a752c9c55b43045219d14b7d5eceb982
Katie McLaughlin <katie@glasnt.com>
2020-01-15 11:27:07 +1000

    bump version

--- setup.py
+++ setup.py
@@ -3,13 +3,9 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
-# TODO - fix so this works
-# with open("requirements.txt", "r") as f:
-#    requirements = f.read()
-
 setuptools.setup(
     name="ih",
-    version="0.0.0-dev24",
+    version="0.0.0-dev25",
     author="Katie McLaughlin",
     author_email="katie@glasnt.com",
     description="A very persuasive package, for creating embroidery patterns",
