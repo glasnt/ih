5f4df52a3822893818b208dfcdde704139008b28
Katie McLaughlin <katie@glasnt.com>
2020-10-25 18:37:34 +1100

    make tests more verbose

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,19 @@
 from click.testing import CliRunner
 from ih.cli import main
+from ih import palette
 
 
 TEST_IMAGE = "test_image.png"
 TEST_HTML = TEST_IMAGE.split(".")[0] + ".html"
 
 
-def runner(args):
+def runner(args, output=TEST_HTML, print_output=False):
     runner = CliRunner()
     result = runner.invoke(main, [TEST_IMAGE] + args)
+    if print_output: 
+        print(result.output)
     assert result.exit_code == 0
-    assert result.output == f"Result: {TEST_HTML}\n"
+    assert output in result.output
 
 
 def test_image():
@@ -21,5 +24,17 @@ def test_render():
     runner(["-r"])
 
 
+def test_palettes():
+    for p in palette.PALETTES:
+        runner(["-p", p])
+
+
 def test_guidelines():
     runner(["-g"])
+
+
+def test_term():
+    runner(["-o", "term"], output="ih version")
+
+def test_term_render():
+    runner(["-o", "term", "-r"], output="ih version", print_output=True)
