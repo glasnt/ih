e06332908ff344dd09a972482e6d657722133417
Katie McLaughlin <katie@glasnt.com>
2021-03-14 21:27:10 +1100

    Not everyone is glasnt

--- demo/update_render.js
+++ demo/update_render.js
@@ -1,13 +1,13 @@
 // npm -i puppeteer
 // takes a screenshot in headless chrome
+// presumes ih has been run to produce the demo_image.html file
 
 const puppeteer = require('puppeteer');
-
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({width: 1330,height: 800});
-  await page.goto('file:///Users/glasnt/git/glasnt/ih/demo/demo_image.html');
+  await page.goto('file://'+process.cwd()+'/demo/demo_image.html');
   await page.screenshot({ path: 'demo/demo_render.png' });
 
   await browser.close();
