df5dc7b66b9dc2c71c03b8cdf5a5bae340c43740
Katie McLaughlin <katie@glasnt.com>
2021-03-14 21:04:27 +1100

    update cli usage

--- demo/update.sh
+++ demo/update.sh
@@ -12,17 +12,27 @@ then
   echo "processing"
   sleep 1
 
+  echo "Update CLI usage"
   python demo/update_readme.py
 
+  git add . && git commit -m "update cli usage"
+
+  echo "bump"
   git version-bump $update
 
-  ih -p alpaca -r -c 4 demo/demo_image.png -f demo/
+  echo "use new version"
+  source venv/bin/activate
+  pip install -e . 
+  ih --version
+  ih -p alpaca -r -c 4 demo/demo_image.png -f demo/ 
+  deactivate 
 
+  echo "Update demo render"
   node demo/update_render.js
 
-  git add . && git commit -m "update"
+  git add . && git commit -m "update demo render"
 
-  echo git release
+  git release
 else
   echo "Not continuing"
 
